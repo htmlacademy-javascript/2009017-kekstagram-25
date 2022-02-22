e8420a1191f98982861762c82a9c496233d620b2
Anna <Lina_89_05@mail.ru>
2022-02-22 20:06:44 +0500

    Добавила функцию случайного целого числа

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function getRandomNumber (min, max) {
+  if (min < max && min >= 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return 'Неправильное число';
+}
+getRandomNumber (4, 10);
+
