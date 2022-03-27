f5a3da1b636c7d7f95649e1fa0144cbbfad0c6dc
Anna Kuliapina <Lina_89_05@mail.ru>
2022-03-28 00:19:19 +0300

    Сделала отдельный модуль для большого фото

--- js/open-photo.js
+++ js/open-photo.js
@@ -1,11 +1,3 @@
-import {createData} from './data.js';
-import { openBigPicture } from './open-photo.js';
-
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const pictureData = createData();
-const pictures = document.querySelector('.pictures');
-
-/*
 const bigPicture = document.querySelector('.big-picture');
 
 const bigPictureClose = document.querySelector('.big-picture__cancel');
@@ -59,19 +51,4 @@ const openBigPicture = function (dataId) {
 
   document.addEventListener('keydown', onPictureEscKeydown);
 };
-*/
-
-const pictureFragment = document.createDocumentFragment();
-
-pictureData.forEach((item) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = item.url;
-  pictureElement.querySelector('.picture__likes').textContent = item.likes;
-  pictureElement.querySelector('.picture__comments').textContent = item.comments.length;
-  pictureElement.addEventListener('click', () => {
-    openBigPicture(item);
-  });
-  pictureFragment.appendChild(pictureElement);
-});
-
-pictures.appendChild(pictureFragment);
+export {openBigPicture};
